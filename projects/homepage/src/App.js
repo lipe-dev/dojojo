dc9d9eb8256fcf7d525fe62e9c88104bb5af93aa
Felipe Pereira <felipe.pereira@loadsmart.com>
2021-05-12 21:57:06 -0300

    feat(homepage): add homepage project

---
+++ projects/homepage/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Dojojo - TDD Front-End Dojo
+        </h1>
+      </header>
+      <section>
+        <h2>Projects</h2>
+
+        <ul>
+          <li><a href="random-pudim">Random Pudim</a></li>
+          <li><a href="game-searcher">Game Searcher</a></li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default App;
